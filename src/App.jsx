467dbc1072b36346a09777cf524756b98f1a75d3
Anthony Guerrero <krezket@Anthonys-Mac-Studio.local>
2024-02-01 12:00:43 -0800

    removed redundant code

--- src/App.jsx
+++ src/App.jsx
@@ -59,23 +59,6 @@ function App() {
         console.log(err);
       });
   }, []);
-  // useEffect(() => {
-  //   const { pathname } = window.location
-  //   const paths = pathname.split("/").filter(entry => entry !== "");
-  //   const lastPath = paths[paths.length - 1]
-  //   console.log(lastPath)
-
-  //   API.getPage(lastPath)
-  //     .then((data) => {
-  //       console.log("bruh:",data)
-  //       setPage(data)
-  //     })
-  //     .catch((err) => {
-  //       console.log("oh noes");
-  //       console.log(err);
-  //     });
-  // }, []);
-
 
   useEffect(() => {
     const storedToken = window.sessionStorage.getItem("token");
@@ -103,9 +86,6 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<Enter />}></Route> */}
-
-        {/* <Route path={window.location.pathname} element={<LoadingPage/>}></Route> */}
 
         {/* HOME HOME HOME HOME */}
         <Route path="/" element={
