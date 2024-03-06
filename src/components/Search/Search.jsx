2614c94f73854306c22c4b1f7e189aeb68b902c6
Anthony Work PC <anthonyguerrero62@outlook.com>
2024-03-06 11:44:15 -0800

    moved search to header

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './search.css'
 import API from '../../utils/API';
 
 export default function Search() {
