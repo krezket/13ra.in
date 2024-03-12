db483052ae38268dba9ee3a29cec72ce31b5a9f6
Anthony Work PC <anthonyguerrero62@outlook.com>
2024-03-12 09:39:20 -0700

    fixed post css

--- src/pages/UserPage/UserPost.jsx
+++ src/pages/UserPage/UserPost.jsx
@@ -17,51 +17,58 @@ export default function UserPage(props) {
   console.log("NEWPAGE:", newPage)
 
   // console.log(newPage.users.username)
-  
-    useEffect(() => {
-      API.getPage(path)
-        .then((data) => {
-          // console.log("REALPAGEDATA:",data)
-          setNewPage(data)
-        })
-        .catch((err) => {
-            console.log("oh noes");
-            console.log(err);
-        }); 
-    }, []);
+
+  useEffect(() => {
+    API.getPage(path)
+      .then((data) => {
+        // console.log("REALPAGEDATA:",data)
+        setNewPage(data)
+      })
+      .catch((err) => {
+        console.log("oh noes");
+        console.log(err);
+      });
+  }, []);
 
   return (
     <>
-      <>
-        <Header
-          type={props.type}
-          username={props.username}
-          userId={props.userId}
-          setUserId={props.setUserId}
-          setEmail={props.setEmail}
-          setUsername={props.setUsername}
-          setToken={props.setToken}
-        />
-      </>
-      <main className='page-main'>
 
-        <div className='title-div'>
-          <h1 className='page-title'>{newPage.title}</h1>
-        </div>
+      <Header
+        type={props.type}
+        username={props.username}
+        userId={props.userId}
+        setUserId={props.setUserId}
+        setEmail={props.setEmail}
+        setUsername={props.setUsername}
+        setToken={props.setToken}
+      />
 
-        <div className='username-div'>
-          <h3 className='page-username'>
-            {/* <Link to={"/" + newPage.users.username} id='user-link'>{newPage.users.username}</Link> */}
-          </h3>
-        </div>
+      <div className="main-con">
+        <main className='page-main'>
 
-        <p className='page-text'>{newPage.text}</p>
+          <div className='title-div'>
+            <h1 className='page-title'>{newPage.title}</h1>
+          </div>
 
-        <div className='date-div'>
-          <p className='date-created'><DayJS className="dayjs" format="M/D/YYYY h:mm a">{props.createdAt}</DayJS></p>
-        </div>
+          <div className='username-div'>
+            <h3 className='page-username'>
+              {/* <Link to={"/" + newPage.users.username} id='user-link'>{newPage.users.username}</Link> */}
+            </h3>
+          </div>
 
-      </main>
+          <p className='page-text'>{newPage.text}</p>
+
+          <div className='date-div'>
+            <p className='date-created'>
+              <DayJS className="dayjs" format="M/D/YYYY h:mm a">
+                {newPage.createdAt}
+              </DayJS>
+            </p>
+          </div>
+
+        </main>
+
+      </div>
       <Footer />
     </>
   )
