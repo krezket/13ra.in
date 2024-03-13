9907ea7413c89c7a37497f95d24bcff6e8ff5a10
Anthony Guerrero <krezket@Anthonys-Mac-Studio.local>
2024-03-12 20:28:14 -0700

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: "/13ra.in/",
-  plugins: [react()],
+  plugins: [react(), pluginRewriteAll()],
 })
