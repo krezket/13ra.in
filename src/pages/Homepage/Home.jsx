e0c4513f19761ad13a4c853abd1e4cb16a2e1d7e
Anthony Guerrero <anthonyguerrero62@gmail.com>
2024-08-17 10:04:56 -0700

    added gifs, adjusted grid layout

--- src/pages/Homepage/Home.jsx
+++ src/pages/Homepage/Home.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Header from "../../components/Header";
 import Footer from '../../components/Footer/';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import Book from '../../assets/book0.gif';
+import Portfolio from '../../assets/cats/cat-2.gif';
 import './style.css';
 
 export default function Home(props) {
@@ -17,7 +19,40 @@ export default function Home(props) {
             />
 
             <main className="main">
-                <Link to='/blog'>Blog</Link>
+                <Link to="/blog" className="grid-item">
+                    <img src={Book} alt='Blog'/>
+                    <p>Blog</p>
+                </Link>
+
+                <Link to="https://krezket.net" className="grid-item">
+                    <img src={Portfolio} alt='Portfolio'/>
+                    <p>Portfolio</p>
+                </Link>
+
+                <Link to="" className="grid-item">
+                </Link>
+
+                <Link to="" className="grid-item">
+                </Link>
+
+                <Link to="" className="grid-item">
+                </Link>
+
+                <Link to="" className="grid-item">
+                </Link>
+
+                <Link to="" className="grid-item">
+                </Link>
+
+                <Link to="" className="grid-item">
+                </Link>
+
+                <Link to="" className="grid-item">
+                </Link>
+
+                <Link to="" className="grid-item">
+                </Link>
+
             </main>
 
             <Footer />
